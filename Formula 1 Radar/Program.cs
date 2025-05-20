0f30513e36bddaded2dc71ae04d6d1121c1516b4
mihai <mihai.marinescu2112@yahoo.com>
2025-05-20 14:11:05 +0300

    Initial commit

---
+++ Formula 1 Radar/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading.Tasks;
+using F1R.core.data_acquisition;
+
+class Program
+{
+    public static int nr = 0;
+    public static void Main(string[] args)
+    {
+        if(nr == 0)
+            DataAcq.LiveDataAcq();
+        else
+        {
+            DataAcq.SimulationDataAcq();
+        }
+        
+    }
+}
+
+
+
+
---
+++ Formula 1 Radar/core/data_acquisition/data_acq.cs
@@ -0,0 +1,6 @@
+﻿namespace F1R.core.data_acquisition;
+
+public class data_acq
+{
+    
+}
