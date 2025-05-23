5f7d50ef9b7d25a5b8964f2c30f7f057dfb644bb
mihai <mihai.marinescu2112@yahoo.com>
2025-05-23 15:46:02 +0300

    SignalR setup

---
+++ Formula 1 Radar/core/hubs/LiveHub.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace F1R.core.hubs;
+
+public class LiveHub : Hub
+{
+    public override Task OnConnectedAsync()
+    {
+        Console.WriteLine($"[SignalR] Client connected: {Context.ConnectionId}");
+        return base.OnConnectedAsync();
+    }
+
+    public override Task OnDisconnectedAsync(Exception? exception)
+    {
+        Console.WriteLine($"[SignalR] Client disconnected: {Context.ConnectionId}");
+        return base.OnDisconnectedAsync(exception);
+    }
+}
