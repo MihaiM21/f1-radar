76f18f9395ed35c90e8d2b8014679a8895f571b7
mihai <mihai.marinescu2112@yahoo.com>
2025-07-02 20:02:26 +0300

    Driver and Data Manager added for data storage and organization.

---
+++ Formula 1 Radar/core/storage_cache/DataManager.cs
@@ -0,0 +1,103 @@
+﻿namespace F1R.core.storage_cache;
+
+public class DataManager
+{
+    private static readonly Dictionary<int, Driver> drivers = new();
+
+    // Cheama functia la inceputul rularii aplicatiei NU UITA !!!!
+    public static void EnsureDriverExists(int driverNumber)
+    {
+        if (!drivers.ContainsKey(driverNumber))
+        {
+            drivers[driverNumber] = new Driver
+            {
+                DriverNumber = driverNumber,
+                Position = (0, 0, 0),
+                BestLap = 0.0,
+                LastLap = 0.0,
+                SectorTimes = (0, 0, 0),
+                SectorStatus = ("Yellow", "Yellow", "Yellow"),
+                S1SegmentStatus = ("Yellow", "Yellow", "Yellow", "Yellow", "Yellow", "Yellow", "Yellow"),
+                S2SegmentStatus = ("Yellow", "Yellow", "Yellow", "Yellow", "Yellow", "Yellow", "Yellow"),
+                S3SegmentStatus = ("Yellow", "Yellow", "Yellow", "Yellow", "Yellow", "Yellow", "Yellow"),
+                SpeedTrapSpeed = 0.0
+            };
+        }
+    }
+
+    public static Driver GetDriver(int driverNumber)
+    {
+        EnsureDriverExists(driverNumber);
+        if (drivers.TryGetValue(driverNumber, out var driver))
+            return driver;
+        return null;
+    }
+
+    public static void UpdateBestLap(int driverNumber, double bestLap)
+    {
+        EnsureDriverExists(driverNumber);
+        if (drivers.ContainsKey(driverNumber))
+            drivers[driverNumber].BestLap = bestLap;
+    }
+
+    public static void UpdatePosition(int driverNumber, int x, int y, int z)
+    {
+        EnsureDriverExists(driverNumber);
+        if (drivers.TryGetValue(driverNumber, out var driver))
+            driver.Position = (x, y, z);
+    }
+
+    public static void UpdateLastLap(int driverNumber, double lastLap)
+    {
+        EnsureDriverExists(driverNumber);
+        if (drivers.ContainsKey(driverNumber))
+        {
+            drivers[driverNumber].LastLap = lastLap;
+        }
+    }
+    
+    public static void UpdateSectorTimes(int driverNumber, (double, double, double) sectorTimes)
+    {
+        EnsureDriverExists(driverNumber);
+        if (drivers.ContainsKey(driverNumber))
+        {
+            drivers[driverNumber].SectorTimes = sectorTimes;
+        }
+    }
+    
+    public static void UpdateSectorStatus(int driverNumber, (string, string, string) sectorStatus)
+    {
+        EnsureDriverExists(driverNumber);
+        if (drivers.ContainsKey(driverNumber))
+        {
+            drivers[driverNumber].SectorStatus = sectorStatus;
+        }
+    }
+    
+    public static void UpdateS1SegmentStatus(int driverNumber, (string, string, string, string, string, string, string) s1SegmentStatus)
+    {
+        EnsureDriverExists(driverNumber);
+        if (drivers.ContainsKey(driverNumber))
+        {
+            drivers[driverNumber].S1SegmentStatus = s1SegmentStatus;
+        }
+    }
+    
+    public static void UpdateS2SegmentStatus(int driverNumber, (string, string, string, string, string, string, string) s2SegmentStatus)
+    {
+        EnsureDriverExists(driverNumber);
+        if (drivers.ContainsKey(driverNumber))
+        {
+            drivers[driverNumber].S2SegmentStatus = s2SegmentStatus;
+        }
+    }
+    
+    public static void UpdateS3SegmentStatus(int driverNumber, (string, string, string, string, string, string, string) s3SegmentStatus)
+    {
+        EnsureDriverExists(driverNumber);
+        if (drivers.ContainsKey(driverNumber))
+        {
+            drivers[driverNumber].S3SegmentStatus = s3SegmentStatus;
+        }
+    }
+}
--- Formula 1 Radar/core/storage_cache/Driver.cs
+++ Formula 1 Radar/core/storage_cache/Driver.cs
@@ -1,14 +1,18 @@
 ﻿namespace F1R.core.storage_cache;
+using System.ComponentModel.DataAnnotations;
 
 public class Driver
 {
+    [Key]
     public int DriverNumber { get; set; }
     public (int X, int Y, int Z) Position { get; set; }
     public double BestLap { get; set; }
     public double LastLap { get; set; }
-    public double Sector1Time { get; set; }
-    public double Sector2Time { get; set; }
-    public double Sector3Time { get; set; }
+    public (double S1, double S2, double S3) SectorTimes { get; set; }
+    public (string S1, string S2, string S3) SectorStatus { get; set; }
+    public (string s1, string s2, string s3, string s4, string s5, string s6, string s7) S1SegmentStatus { get; set; }
+    public (string s1, string s2, string s3, string s4, string s5, string s6, string s7) S2SegmentStatus { get; set; }
+    public (string s1, string s2, string s3, string s4, string s5, string s6, string s7) S3SegmentStatus { get; set; }
     public List<int> Sector1MiniSectors { get; set; } = new();
     public List<int> Sector2MiniSectors { get; set; } = new();
     public List<int> Sector3MiniSectors { get; set; } = new();
